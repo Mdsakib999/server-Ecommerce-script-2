11d38cc568755a46fd424210419e663d19f8f2dd
Md. Shahidul Islam <shanto57575@gmail.com>
2025-09-08 19:00:47 +0600

    auth functionality added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,8 +1,27 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "../user/user.controller";
+import passport from "passport";
+import { AuthController } from "./auth.controller";
 
 const router = Router();
 
-router.post("/register", UserControllers.createUser);
+router.get(
+  "/google",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const redirect = req.query.redirect || "/";
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+      state: redirect as string,
+    })(req, res, next);
+  }
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}/login?error=There is some issues with your account. Please contact with out support team!`,
+  }),
+  AuthController.googleCallbackController
+);
 
 export const authRoutes = router;
